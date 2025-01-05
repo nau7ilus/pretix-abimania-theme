d937429608d9c795380797b90a801ab23fa802f6
nau7ilus <filip@zelinskyi.de>
2025-01-05 02:39:08 +0100

    feat(templates): added front_page_top

--- pretix_abimania_theme/signals.py
+++ pretix_abimania_theme/signals.py
@@ -1,1 +1,9 @@
-# Register your receivers here
+from django.dispatch import receiver
+from django.template.loader import get_template
+from pretix.presale.signals import front_page_top
+from pretix.base.models import Event
+
+@receiver(front_page_top, dispatch_uid="abimania_front_page_top")
+def render_seating_plan(sender: Event, request=None, **kwargs):
+    template = get_template("pretix_abimania_theme/front_page_top.html")
+    return template.render({"event": sender})
